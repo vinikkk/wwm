8c95ef48baedcb816043fb16593c7c9c03160eed
Vinicius Pereira <vinipereira3@live.com>
2021-11-30 08:31:20 +0000

    Update Utility.cs

--- Browsers/Unity/Nexus Browser/Assets/Scripts/Utility.cs
+++ Browsers/Unity/Nexus Browser/Assets/Scripts/Utility.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 using UnityEngine.Networking;
+using Siccity.GLTFUtility;
 
 public class Utility
 {
@@ -30,7 +31,7 @@ public class Utility
 		return (R)result;
 	}
 
-	public static IEnumerator GetData(string url, Action<string> callback)
+	public static IEnumerator GetData(string url, Action<object> callback)
 	{
 		UnityWebRequest www = UnityWebRequest.Get(url);
 		www.useHttpContinue = false;
@@ -44,5 +45,50 @@ public class Utility
 		{
 			callback?.Invoke(www.downloadHandler.text);
 		}
+	}
+
+	/// <summary>
+	/// Download a file into the disk
+	/// </summary>
+	/// <param name="url"></param>
+	/// <param name="callback"></param>
+	/// <returns></returns>
+	public static IEnumerator DownloadFile(string url, Action<string> callback)
+	{
+		string fileName = url.Substring(url.LastIndexOf('/') + 1);
+		string fileDirectory = Application.persistentDataPath + "/Data/";
+
+		Debug.Log($"Downloading file:\n{fileName} => {fileDirectory}");
+
+		//If exists return and invoke the callback, if not proceed to download
+		if (System.IO.File.Exists(fileDirectory + fileName))
+		{																	 
+			callback?.Invoke(fileDirectory + fileName);
+			yield break; 
+		}
+
+		//Create directory if it does not exist
+		System.IO.Directory.CreateDirectory(fileDirectory);
+
+		//Download File
+		UnityWebRequest www = UnityWebRequest.Get(url);
+		www.useHttpContinue = false;
+		yield return www.SendWebRequest();
+
+		if (www.result != UnityWebRequest.Result.Success)
+		{
+			Debug.Log(www.error);
+		}
+		else
+		{
+			System.IO.File.WriteAllBytes(fileDirectory + fileName, www.downloadHandler.data);
+
+			callback?.Invoke(fileDirectory + fileName);
+		}
+	}
+
+	public static void LoadMesh(string path, Action<GameObject, AnimationClip[]> callback)
+	{
+		Importer.ImportGLBAsync(path, new ImportSettings(), callback);
 	}
 }
